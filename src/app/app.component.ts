2c2aabef4a05348535f1d35402ec66211b98776d
Salmanulfaris-M <fsalmanul13@gmail.com>
2025-05-20 23:57:22 +0530

    Added favicon for dark and light mode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -7,6 +8,27 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'salman';
+  isBrowser: boolean = false;
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {
+    this.isBrowser = isPlatformBrowser(this.platformId);
+  } ngOnInit(): void {
+
+    if (this.isBrowser) {
+      this.updateFavicon();
+      // Optional: Update favicon if the user switches theme at runtime
+      window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', () => {
+        this.updateFavicon();
+      });
+    }
+  }
+
+  updateFavicon(): void {
+    const isDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    const favicon: HTMLLinkElement | null = document.querySelector('#favicon');
+    if (favicon) {
+      favicon.href = isDark ? 'icon-w.svg' : 'icon-b.svg';
+    }
+  }
 }
